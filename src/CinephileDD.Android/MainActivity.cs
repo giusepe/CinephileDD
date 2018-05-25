c79c7743f6e6daa7ffcc1f6172fe9ccad5c1506d
Giusepe Casagrande <giusepe@gmail.com>
2018-05-25 13:41:39 +0200

    Initial Commit

---
+++ src/CinephileDD.Android/MainActivity.cs
@@ -0,0 +1,28 @@
+﻿using Akavache;
+using Android.App;
+using Android.Content.PM;
+using Android.OS;
+using FFImageLoading.Forms.Droid;
+
+namespace CinephileDD.Droid
+{
+	[Activity(Label = "CinephileDD", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            TabLayoutResource = Resource.Layout.Tabbar;
+            ToolbarResource = Resource.Layout.Toolbar;
+
+            base.OnCreate(bundle);
+
+            global::Xamarin.Forms.Forms.Init(this, bundle);
+
+			BlobCache.ApplicationName = "Cinephile";
+            CachedImageRenderer.Init();
+
+            LoadApplication(new App());
+        }
+    }
+}
+
